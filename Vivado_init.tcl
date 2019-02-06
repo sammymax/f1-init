2b23df5a99d0f9471fb9b548cce547b72d3afbf5
sammymax <philwsun@gmail.com>
2019-02-06 21:41:49 +0000

    Source hlx_setup.tcl from the right place

--- Vivado_init.tcl
+++ Vivado_init.tcl
@@ -1,2 +1,2 @@
-source /home/centos/aws-fpga/hdk/common/shell_v04261818/hlx/hlx_setup.tcl
+source /home/centos/src/project_data/aws-fpga/hdk/common/shell_v04261818/hlx/hlx_setup.tcl
 set_param board.repoPaths /home/centos/aws-fpga/hdk/common/shell_v04261818/hlx/design/
