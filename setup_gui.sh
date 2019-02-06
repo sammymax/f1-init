a7f7f9b4242ecf78161ef56290cd9dc0df399cda
sammymax <philwsun@gmail.com>
2019-02-06 21:00:43 +0000

    Update setup_gui.sh script to work
    Stuff taken from https://s3.amazonaws.com/aws-fpga-developer-ami/1.5.0/Scripts/setup_gui.sh

--- setup_gui.sh
+++ setup_gui.sh
@@ -1,31 +1,35 @@
 #!/bin/bash
+set -e
 
 CENTOS_HOME=/home/centos
-CENTOS_PWD=temp_pwd_
-CENTOS_PWD+=$((RANDOM%1000))
 
 pushd $CENTOS_HOME
 
-function setup_password {
-    info_msg "Setting password for user 'centos'"
-    echo "centos:$CENTOS_PWD" | sudo chpasswd
-
-    info_msg "**************************************"
-    info_msg "*** PASSWORD : ${CENTOS_PWD}   ****"
-    info_msg "**************************************"
-}
-
 function setup_gui {
     info_msg "Setting up GUI packages"
-
-    sudo yum install -y kernel-devel # Needed to re-build ENA driver
-    sudo yum groupinstall -y "Server with GUI"
-    sudo systemctl set-default graphical.target
-
+    ker_ver=kernel-devel-
+    ker_ver+=$(uname -r)
+    echo $ker_ver
+    sudo yum install -y $ker_ver # Needed to re-build ENA driver
+    sudo yum update grub2 -y
     sudo yum -y install epel-release
+
+    sudo yum groupinstall -y "Server with GUI"
+    sudo yum groupinstall -y "MATE Desktop"
+    sudo yum groupinstall -y "GNOME Desktop"
+
+    sudo systemctl set-default graphical.target
+    
     sudo rpm -Uvh http://li.nux.ro/download/nux/dextop/el7/x86_64/nux-dextop-release-0-5.el7.nux.noarch.rpm
+    sudo yum -y install -y xorgxrdp
     sudo yum install -y xrdp tigervnc-server
-    sudo yum install -y chromium
+
+    cat > /home/centos/.Xclients << EOL
+#!/bin/bash
+exec mate-session
+EOL
+    chmod +x /home/centos/.Xclients
+
     sudo systemctl start xrdp
     sudo systemctl enable xrdp
     sudo systemctl disable firewalld
@@ -41,6 +45,5 @@ function err_msg {
 }
 
 setup_gui
-setup_password
 
 popd
